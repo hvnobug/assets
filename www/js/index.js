dcd54541c59979743325b172f2ffcd6accfd0ac2
Emil <972080809@qq.com>
2019-11-20 13:05:08 +0800

    2019-11-20

--- www/js/index.js
+++ www/js/index.js
@@ -1,1 +1,95 @@
-function click(e){if(document.all){if(event.button==2||event.button==3){oncontextmenu='return false'}}if(document.layers){if(e.which==3){oncontextmenu='return false'}}}if(document.layers){document.captureEvents(Event.MOUSEDOWN)}document.onmousedown=click;document.oncontextmenu=new Function("return false;")document.onkeydown=document.onkeyup=document.onkeypress=function(){if(window.event.keyCode==123){window.event.returnValue=false;return(false)}}
+// 屏蔽F12 审查元素
+document.onkeydown = function () {
+    if (window.event && window.event.keyCode == 123) {
+        console.info("F12被禁用");
+        event.keyCode = 0;
+        event.returnValue = false;
+    }
+    if (window.event && window.event.keyCode == 13) {
+        window.event.keyCode = 505;
+    }
+    if (window.event && window.event.keyCode == 8) {
+        console.info("请使用Del键进行字符的删除操作！");
+        window.event.returnValue = false;
+    }
+};
+
+// 屏蔽右键菜单
+document.oncontextmenu = function (event) {
+    if (window.event) {
+        event = window.event;
+    }
+    try {
+        var the = event.srcElement;
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+            return false;
+        }
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+// 屏蔽粘贴
+document.onpaste = function (event) {
+    if (window.event) {
+        event = window.event;
+    }
+    try {
+        var the = event.srcElement;
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+            return false;
+        }
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+// 屏蔽复制
+document.oncopy = function (event) {
+    if (window.event) {
+        event = window.event;
+    }
+    try {
+        var the = event.srcElement;
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+            return false;
+        }
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+// 屏蔽剪切
+document.oncut = function (event) {
+    if (window.event) {
+        event = window.event;
+    }
+    try {
+        var the = event.srcElement;
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+            return false;
+        }
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+// 屏蔽选中
+document.onselectstart = function (event) {
+    if (window.event) {
+        event = window.event;
+    }
+    try {
+        var the = event.srcElement;
+        if (!((the.tagName == "INPUT" && the.type.toLowerCase() == "text") || the.tagName == "TEXTAREA")) {
+            return false;
+        }
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
